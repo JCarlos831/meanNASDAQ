58cab6e3f1911b10cb4bb77bd7b6b536428269ec
Juan Montoya <jcmontoya@gmail.com>
2018-03-20 20:28:25 +0000

    Added Get all and Get one stock controllers.

--- MEAN/api/controllers/stocks.controllers.js
+++ MEAN/api/controllers/stocks.controllers.js
@@ -2,7 +2,31 @@ var stockData = require('../data/nasdaqCompanyList.json');
 
 module.exports.stocksGetAll = function(req, res){
         console.log("GET the stocks");
+        console.log(req.query);
+        
+        var offset = 5;
+        var count = 5;
+        
+        if(req.query && req.query.offset){
+            offset = parseInt(req.query.offset, 10);
+        }
+        
+        if(req.query && req.query.count){
+            count = parseInt(req.query.count, 10);
+        }        
+        
+        var returnData = stockData.slice(offset,offset+count);
+        
         res
             .status(200)
-            .json( stockData );  
+            .json( returnData );  
+};
+
+module.exports.stocksGetOne = function(req, res){
+    var stockId = req.params.stockId;
+    var thisStock = stockData[stockId];
+        console.log("GET stockId", stockId);
+        res
+            .status(200)
+            .json( thisStock );  
 };
--- MEAN/api/routes/index.js
+++ MEAN/api/routes/index.js
@@ -6,5 +6,9 @@ var ctrlStocks = require('../controllers/stocks.controllers.js');
 router
     .route('/stocks')
     .get(ctrlStocks.stocksGetAll);
+    
+router
+    .route('/stocks/:stockId')
+    .get(ctrlStocks.stocksGetOne);
 
 module.exports = router;
